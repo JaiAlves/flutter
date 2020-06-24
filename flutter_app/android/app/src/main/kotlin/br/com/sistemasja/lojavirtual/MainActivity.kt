40bdb28d7f5ca71f814dfacd71d30e4b59ac5d94
unknown <jailson.alves@quicard.com.br>
2020-06-23 22:53:15 -0300

    feito ate a tela de login, falta validar

---
+++ flutter_app/android/app/src/main/kotlin/br/com/sistemasja/lojavirtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sistemasja.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/lib/screens/base/base_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
+import 'package:lojavirtual/models/page_manager.dart';
+import 'package:lojavirtual/screens/login/login_screen.dart';
+import 'package:provider/provider.dart';
+
+
+class BaseScreen extends  StatelessWidget{
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider (
+      create: (_)=> PageManager(pageController),
+        child : PageView(
+          controller: pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            LoginScreen(),
+            Scaffold(
+              drawer: CustomDrawer(),
+              appBar: AppBar(
+                title: const Text('Home2'),
+              ),
+            ),
+            Scaffold(
+              drawer: CustomDrawer(),
+              appBar: AppBar(
+                title: const Text('Home3'),
+              ),
+            ),
+            Scaffold(
+              drawer: CustomDrawer(),
+              appBar: AppBar(
+                title: const Text('Home44'),
+              ),
+            ),
+            Container(color: Colors.red,),
+            Container(color: Colors.yellow,),
+            Container(color: Colors.green,),
+          ],
+        )
+    );
+  }
+
+}
